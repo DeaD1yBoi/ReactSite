64c622f80310186e4882cd8bcc89bd10eb02b700
DeaD1y <kulish233@Gmail.com>
2023-01-30 15:39:37 +0200

    75 lesson

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import {Field, Form, Formik} from "formik";
 
-const Login = (props) => {
-    return <h1>LOGIN</h1>
-}
+const Login = (props) => (
+    <div>
+        <h1>Login</h1>
+        <Formik
+            initialValues={{
+                login: '',
+                password: '',
+                rememberMe: '',
+            }}
+            onSubmit={(values) => {
+                alert(JSON.stringify(values, null, 2));
+                debugger
+            }}
+        >
+            <Form>
+                <div>
+                    <label htmlFor="login">Login</label>
+                    <Field id="login" name="login" placeholder="Login"/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <Field id="password" name="password" placeholder="Password"/>
+                </div>
+                <div>
+                    <label htmlFor="RememberMe">Remember Me</label>
+                    <Field type={'checkbox'} name={'rememberMe'}/>
+                </div>
+                <div>
+                    <button type="login">Login</button>
+                </div>
+            </Form>
+        </Formik>
+    </div>
+);
+
 
 export default Login;
